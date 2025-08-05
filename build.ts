32ae9cf7b16debbbe9104515b3587ffbda9629d1
Mr Martian <craiggles@s2maps.io>
2025-08-05 02:18:28 -0400

    fixed building

--- build.ts
+++ build.ts
@@ -8,30 +8,30 @@ try {
     format: 'esm',
     minify: true,
     sourcemap: 'external',
-    target: 'browser',
+    target: 'node',
     // target: 'esnext', // Adjust target based on your project needs
   });
   console.info('Node Build completed successfully!', outputNode);
-  const outputFile = await bun.build({
-    entrypoints: ['src/file.ts'],
-    outdir: 'dist',
-    format: 'esm',
-    minify: true,
-    sourcemap: 'external',
-    target: 'node',
-    // target: 'esnext', // Adjust target based on your project needs
-  });
-  console.info('File Build completed successfully!', outputFile);
-  const outputMMap = await bun.build({
-    entrypoints: ['src/mmap.ts'],
-    outdir: 'dist',
-    format: 'esm',
-    minify: true,
-    sourcemap: 'external',
-    target: 'node',
-    // target: 'esnext', // Adjust target based on your project needs
-  });
-  console.info('File Build completed successfully!', outputMMap);
+  // const outputFile = await bun.build({
+  //   entrypoints: ['src/file.ts'],
+  //   outdir: 'dist',
+  //   format: 'esm',
+  //   minify: true,
+  //   sourcemap: 'external',
+  //   target: 'node',
+  //   // target: 'esnext', // Adjust target based on your project needs
+  // });
+  // console.info('File Build completed successfully!', outputFile);
+  // const outputMMap = await bun.build({
+  //   entrypoints: ['src/mmap.ts'],
+  //   outdir: 'dist',
+  //   format: 'esm',
+  //   minify: true,
+  //   sourcemap: 'external',
+  //   target: 'node',
+  //   // target: 'esnext', // Adjust target based on your project needs
+  // });
+  // console.info('File Build completed successfully!', outputMMap);
 } catch (error) {
   console.error('Build failed:', error);
 }
